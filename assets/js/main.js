781799ffccd586f823b542d40e5a84b7ed468590
Reinaldo Búrigo Netto <37005414+ReinaldoBurigoNetto@users.noreply.github.com>
2020-10-08 14:34:58 -0300

    novocomit
    atualização

---
+++ assets/js/main.js
@@ -0,0 +1,147 @@
+$(function() {
+    
+    "use strict";
+    
+    //===== Prealoder
+    
+    $(window).on('load', function(event) {
+        $('#preloader').delay(500).fadeOut(500);
+    });
+    
+    
+    //===== Mobile Menu 
+    
+    $(".navbar-toggler").on('click', function() {
+        $(this).toggleClass('active');
+    });
+    
+    $(".navbar-nav a").on('click', function() {
+        $(".navbar-toggler").removeClass('active');
+    });
+    
+    
+    //===== close navbar-collapse when a  clicked
+    
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+    
+    
+    //===== Sticky
+    
+    $(window).on('scroll',function(event) {    
+        var scroll = $(window).scrollTop();
+        if (scroll < 10) {
+            $(".navgition").removeClass("sticky");
+        }else{
+            $(".navgition").addClass("sticky");
+        }
+    });
+    
+    
+    //===== Section Menu Active
+    
+    var scrollLink = $('.page-scroll');
+        // Active link switching
+        $(window).scroll(function() {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function() {
+
+          var sectionOffset = $(this.hash).offset().top - 90;
+
+          if ( sectionOffset <= scrollbarLocation ) {
+            $(this).parent().addClass('active');
+            $(this).parent().siblings().removeClass('active');
+          }
+        });
+    });
+    
+    
+    //====== Magnific Popup
+    
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+        // other options
+    });
+    
+    
+    //===== Back to top
+    
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function(event) {
+        if($(this).scrollTop() > 600){
+            $('.back-to-top').fadeIn(200)
+        } else{
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+    
+    
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+    
+    $(function (){
+        $('.multiple-items').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrowns: true,
+            autoplay: true,
+            autoplaySpeed:2000,
+            dots: false,
+            centerMode: true,
+            centerPadding : '0px',
+
+
+          });
+    });
+
+    
+});
+
+$(function (){
+    $('.responsive').slick({
+        dots: true,
+        infinite: true,
+        speed: 230,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });    
+
+});
